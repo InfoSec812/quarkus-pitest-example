6a5146a0fd37b1916f912625fea2a063e53eadce
Deven Phillips <deven.phillips@redhat.com>
2021-08-04 10:55:52 -0400

    Added minimal test implementation

--- src/test/java/com/redhat/runtimes/api/impl/TodosApiServiceTest.java
+++ src/test/java/com/redhat/runtimes/api/impl/TodosApiServiceTest.java
@@ -1,10 +1,16 @@
 package com.redhat.runtimes.api.impl;
 
+import com.redhat.runtimes.api.TodosApi;
 import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+import javax.ws.rs.core.Response;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TodosApiServiceTest {
+
+	TodosApi underTest = new TodosApiServiceImpl();
 
 	@Test
 	void createTodo() {
@@ -12,6 +18,9 @@ class TodosApiServiceTest {
 
 	@Test
 	void deleteTodo() {
+		Response response = underTest.deleteTodo("failTest", null);
+
+		assertEquals(response.getStatus(), 200);
 	}
 
 	@Test
